cee5b2ed4ce03615cdfba5fa844c3f9a6a0c2c12
Rahulrockss <rahulskumar343@gmail.com>
2024-06-07 23:44:34 +0530

    commit

--- client/redux/URI.tsx
+++ client/redux/URI.tsx
@@ -3,9 +3,9 @@ import {Platform} from 'react-native';
 let URI = '';
 
 if (Platform.OS === 'ios') {
-  URI = 'https://thread-clone-pink-nu.vercel.app/api/v1';
+  URI = 'https://thread-clone-etlw6tjba-rahul-kumars-projects-07850f38.vercel.app/api/v1';
 } else {
-  URI = 'https://thread-clone-pink-nu.vercel.app/api/v1';
+  URI = 'https://thread-clone-etlw6tjba-rahul-kumars-projects-07850f38.vercel.app/api/v1';
 }
 
 export {URI};
