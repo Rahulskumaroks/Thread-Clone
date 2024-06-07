1b6e1d9bd7c1d78f1b0f8d726176acf93c3f4b04
Rahulrockss <rahulskumar343@gmail.com>
2024-06-07 21:33:23 +0530

    Vercel Setup

--- client/redux/URI.tsx
+++ client/redux/URI.tsx
@@ -3,9 +3,9 @@ import {Platform} from 'react-native';
 let URI = '';
 
 if (Platform.OS === 'ios') {
-  URI = '/api/v1';
+  URI = 'https://thread-clone-pink-nu.vercel.app/api/v1';
 } else {
-  URI = 'http://192.168.1.12:8081/api/v1';
+  URI = 'https://thread-clone-pink-nu.vercel.app/api/v1';
 }
 
 export {URI};
